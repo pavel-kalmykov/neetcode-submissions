22e2f76bfbf5ee19ce8d52548f79eba3adfef956
Pável Kalmykov Razgovórov <9091619+pavel-kalmykov@users.noreply.github.com>
2026-05-15 11:24:45 +0200

    Add: climbing-stairs - submission-6

---
+++ Data Structures & Algorithms/climbing-stairs/submission-6.py
@@ -0,0 +1,6 @@
+from functools import cache
+
+class Solution:
+    @cache
+    def climbStairs(self, n: int) -> int:
+        return n if n <= 2 else self.climbStairs(n - 1) + self.climbStairs(n - 2)
